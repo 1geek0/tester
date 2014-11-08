27c406d900f89691f5d697fb99ad76a7b7e139b3
Nilay Kulkarni <typhoscientic@gmail.com>
2014-11-08 19:51:08 +0530

    Added Debug Logs

--- app/src/main/java/com/geekydreams/devicetester/screenService.java
+++ app/src/main/java/com/geekydreams/devicetester/screenService.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
+import android.util.Log;
 
 import com.geekydreams.devicetester.ScreenOnReciever.*;
 
@@ -12,7 +13,8 @@ import com.geekydreams.devicetester.ScreenOnReciever.*;
  * Created by Nilay on 08/11/2014.
  */
 public class screenService extends Service {
-    int myName;
+
+    private String Debug_oncreate;
 
     static long screenOnTime;
     BroadcastReceiver mReceiver = null;
@@ -23,17 +25,20 @@ public class screenService extends Service {
         IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
         mReceiver = new ScreenOnReciever();
         registerReceiver(mReceiver, filter);
+        Log.d(Debug_oncreate,"OnCreate");
     }
 
     @Override
     public void onStart(Intent intent, int startId) {
         while (ScreenOnReciever.screenOn == true) {
+            Log.d(Debug_oncreate, "Goal!");
             screenOnTime++;
         }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
+        Log.d(Debug_oncreate, "Binder");
         return null;
     }
 }
